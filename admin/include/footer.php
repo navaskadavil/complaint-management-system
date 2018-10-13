5f41521cacbc21d411390c4be3182fa5f08c954c
Sukhsagar <sukhsagar.batra@gmail.com>
2018-10-13 13:03:33 -0700

    Initial commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2018 CMS </b> All rights reserved.
+		</div>
+	</div>
---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2018 - Developed by DEBUGGERS Dept.
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
